ff3a959a3c87aa197b837f76edca7421b83dc162
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-05-02 16:50:35 +0530

    HMS Image

--- HMS/react/src/views/Home.jsx
+++ HMS/react/src/views/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './home.css'
+import hmsimg from '../assets/hmsimg.png';
 
 export default function Home() {
   return (
     <div>
 
         <h2>home</h2>
-
+        <img src={hmsimg} alt="HMS Image"/>
     </div>
   )
 }
